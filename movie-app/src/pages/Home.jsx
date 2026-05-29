4c872b965e4b265aabe6a17bea7062f74e3a05c8
Masavi Syed <masavi.aiub@gmail.com>
2026-05-29 00:15:57 -0400

    search Query

--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -1,17 +1,64 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard";
-function Home(){
-    const movies=[
-        {id:1,title:"The Shawshank Redemption",releaseYear:1994,url:"https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg"},
-        {id:2,title:"The Godfather",releaseYear:1972,url:"https://m.media-amazon.com/images/I/41+eK8zBwQL._AC_.jpg"},
-        {id:3,title:"The Dark Knight",releaseYear:2008,url:"https://m.media-amazon.com/images/I/51EbJjlLJGL._AC_.jpg"},
-        {id:4,title:"Pulp Fiction",releaseYear:1994,url:"https://m.media-amazon.com/images/I/51V5ZpFyaFL._AC_.jpg"},
-        {id:5,title:"Forrest Gump",releaseYear:1994,url:"https://m.media-amazon.com/images/I/41c9r0YHnQL._AC_.jpg"},
-    ]
-    return <div className="home">
-        <div className="movies-grid">
-            {movies.map((movie)=>(
-                <MovieCard movie={movie} key={movie.id}/>))}
-        </div>
+function Home() {
+    const[searchQuery,setSearchQuery]=useState("");
+
+  const movies = [
+    {
+      id: 1,
+      title: "The Shawshank Redemption",
+      releaseYear: 1994,
+      url: "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg",
+    },
+    {
+      id: 2,
+      title: "The Godfather",
+      releaseYear: 1972,
+      url: "https://m.media-amazon.com/images/I/41+eK8zBwQL._AC_.jpg",
+    },
+    {
+      id: 3,
+      title: "The Dark Knight",
+      releaseYear: 2008,
+      url: "https://m.media-amazon.com/images/I/51EbJjlLJGL._AC_.jpg",
+    },
+    {
+      id: 4,
+      title: "Pulp Fiction",
+      releaseYear: 1994,
+      url: "https://m.media-amazon.com/images/I/51V5ZpFyaFL._AC_.jpg",
+    },
+    {
+      id: 5,
+      title: "Forrest Gump",
+      releaseYear: 1994,
+      url: "https://m.media-amazon.com/images/I/41c9r0YHnQL._AC_.jpg",
+    },
+  ];
+  const handleSearch = (e) => {
+    e.preventDefault()
+    alert(searchQuery)
+    setSearchQuery("")
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e)=>setSearchQuery(e.target.value)}
+        />
+        <button type="submit"className="search-button">Search</button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
     </div>
+  );
 }
 export default Home;
